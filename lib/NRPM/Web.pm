af46ebb99a32b76deb209c763ec4d2128bd6dfd2
Torsten Raudssus <github@raudssus.de>
2010-12-12 02:09:07 +0100

    First own changes

--- lib/NRPM/Web.pm
+++ lib/NRPM/Web.pm
@@ -4,18 +4,10 @@ use namespace::autoclean;
 
 use Catalyst::Runtime 5.80;
 
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
 use Catalyst qw/
-    -Debug
     ConfigLoader
     Static::Simple
+	Unicode::Encoding
 /;
 
 extends 'Catalyst';
@@ -23,18 +15,8 @@ extends 'Catalyst';
 our $VERSION = '0.01';
 $VERSION = eval $VERSION;
 
-# Configure the application.
-#
-# Note that settings in nrpm_web.conf (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with an external configuration file acting as an override for
-# local deployment.
-
 __PACKAGE__->config(
     name => 'NRPM::Web',
-    # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
 );
 
@@ -44,7 +26,7 @@ __PACKAGE__->setup();
 
 =head1 NAME
 
-NRPM::Web - Catalyst based application
+NRPM::Web - Niederrhein Perl Monger Web application for L<http://niederrhein.pm.org>
 
 =head1 SYNOPSIS
 
@@ -52,20 +34,11 @@ NRPM::Web - Catalyst based application
 
 =head1 DESCRIPTION
 
-[enter your description here]
+This is the web application which is used by the NiederheinPM for running L<http://niederrhein.pm.org>
 
 =head1 SEE ALSO
 
 L<NRPM::Web::Controller::Root>, L<Catalyst>
-
-=head1 AUTHOR
-
-Torsten Raudssus,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
 
 =cut
 
--- lib/NRPM/Web/Controller/Root.pm
+++ lib/NRPM/Web/Controller/Root.pm
@@ -4,10 +4,6 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config(namespace => '');
 
 =head1 NAME
@@ -16,7 +12,7 @@ NRPM::Web::Controller::Root - Root Controller for NRPM::Web
 
 =head1 DESCRIPTION
 
-[enter your description here]
+Root Controller
 
 =head1 METHODS
 
@@ -30,7 +26,7 @@ sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
     # Hello World
-    $c->response->body( $c->welcome_message );
+    $c->response->body( "niederrhein.pm.org" );
 }
 
 =head2 default
@@ -52,17 +48,6 @@ Attempt to render a view, if needed.
 =cut
 
 sub end : ActionClass('RenderView') {}
-
-=head1 AUTHOR
-
-Torsten Raudssus,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 __PACKAGE__->meta->make_immutable;
 
